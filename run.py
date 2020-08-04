3199c03da0bd9f7c6ec320501b12f0ae5a597a7e
Michael Abrahamsen <mail@michaelabrahamsen.com>
2020-08-04 15:48:23 -0600

    redirect base to table view

--- run.py
+++ run.py
@@ -1,9 +1,13 @@
 import os
 from app import app
+from flask import redirect
 from app.routes.routes import stocks_blueprint
 
 app.register_blueprint(stocks_blueprint, url_prefix='/stocks')
 
+@app.route('/')
+def base():
+    return redirect("/stocks/table", code=302)
 
 production = os.environ.get("PRODUCTION", False)
 
