ebd2d944a904c777e1bc605847e49692777b39f9
Michael Abrahamsen <mail@michaelabrahamsen.com>
2020-08-04 13:30:28 -0600

    modify run.py to allow production environment

--- run.py
+++ run.py
@@ -1,7 +1,14 @@
+import os
 from app import app
 from app.routes.routes import stocks_blueprint
 
 app.register_blueprint(stocks_blueprint, url_prefix='/stocks')
 
+
+production = os.environ.get("PRODUCTION", False)
+
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=8000, debug=True)
+    if production:
+        app.run()
+    else:
+        app.run(host='127.0.0.1', port=8000, debug=True)
