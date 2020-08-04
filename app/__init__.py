f1d09189e8916e792d98c7027ccf831074243a4c
Michael Abrahamsen <mail@michaelabrahamsen.com>
2020-08-04 16:13:46 -0600

    allow SQLALCHEMY_DATABASE_URI configuration

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
@@ -7,6 +8,11 @@ from flask_login import LoginManager
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '6c9a0af7a220762b3ec7d435b3ff14a4'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.database'
+
+SQLALCHEMY_DATABASE_URI = os.environ.get(
+    "DATABASE_URI", 'sqlite:///site.database'
+)
+
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
